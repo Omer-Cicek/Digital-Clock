d4dc61df49be946dd710d2858121d56cdee9b393
Omer-Cicek <omercicek160408@gmail.com>
2022-03-07 10:42:16 +0300

    last commit

--- App.js
+++ App.js
@@ -3,14 +3,9 @@ const minutesDiv = document.querySelector('.minutes');
 const secondsDiv = document.querySelector('.seconds');
 const amPmDiv = document.querySelector('.am-pm');
 
-const hours = new Date().getHours().toString().padStart(2, 0);
-const minutes = new Date().getMinutes().toString().padStart(2, 0);
-const seconds = new Date().getSeconds().toString().padStart(2, 0);
-const currentAmPm = hours / 12 < 1 ? 'AM' : 'PM';
-
 setInterval(() => {
   hoursDiv.innerText = `${new Date().getHours().toString().padStart(2, 0)}`;
   minutesDiv.innerText = `${new Date().getMinutes().toString().padStart(2, 0)}`;
   secondsDiv.innerText = `${new Date().getSeconds().toString().padStart(2, 0)}`;
-  amPmDiv.innerText = `${currentAmPm}`;
+  amPmDiv.innerText = `${+hoursDiv.innerText / 12 < 1 ? 'AM' : 'PM'}`;
 }, 1000);
